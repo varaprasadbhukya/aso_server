de69dfaa40ae3fca3df49888b1a212f4cb17dba7
VARA PRASAD <varaprasad.bhavya@gmail.com>
2024-01-04 22:25:18 +0530

    added review fetching

--- Aso.py
+++ Aso.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from google_play_scraper import app as play_scraper_app, search
+from google_play_scraper import app as play_scraper_app, search, Sort, reviews
 
 app = Flask(__name__)
 CORS(app, origins="*")
@@ -32,5 +32,21 @@ def app_data():
 
     return jsonify(result)
 
+@app.route('/reviewsdata', methods=['POST'])
+def reviews_data():
+    data = request.get_json()
+    app_id = data['data']['app_id']
+
+    result, continuation_token = reviews(
+        app_id,
+        sort=Sort.NEWEST,
+    )
+    
+    result, _ = reviews(
+        app_id,
+        continuation_token=continuation_token # defaults to None(load from the beginning)
+    )
+    
+    return jsonify(result)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4040)
